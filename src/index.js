143ddf4e4e654f67e9fde536114114dc278dd81c
tsavlis <tsavlis.thanos@gmail.com>
2019-08-15 21:43:28 +0300

    removes firebase  as it was not needed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet } from "react-native";
 import { RootNavigator } from "./routes";
-import firebase from "firebase";
-
-var firebaseConfig = {
-  apiKey: "AIzaSyB-ji38Tp0U4AUbv08sr5VurmowubvQTY0",
-  authDomain: "cryptonews-6bf7b.firebaseapp.com",
-  databaseURL: "https://cryptonews-6bf7b.firebaseio.com",
-  projectId: "cryptonews-6bf7b",
-  storageBucket: "",
-  messagingSenderId: "650806754807",
-  appId: "1:650806754807:web:d593332e2d475fd0"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   render() {
